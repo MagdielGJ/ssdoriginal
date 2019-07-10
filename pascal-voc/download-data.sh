25e3f9fb53f057accb96c9166a6d89aa59b1c447
Magdiel <mgdlgj@gmail.com>
2019-07-10 11:45:26 -0500

    Update download-data.sh

--- pascal-voc/download-data.sh
+++ pascal-voc/download-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
-wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
-wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+wget -c http://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
+wget -c http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
+wget -c http://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
 
 mkdir -p trainval
 mkdir -p test
